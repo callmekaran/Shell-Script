69e13fd6c4e0be3a9d1ceb8a5d8b856453dc5a2a
callmekaran <69335113+callmekaran@users.noreply.github.com>
2023-07-31 12:12:04 +0530

    Create Backup_For_App_and_Db.sh
    Added Email After the Backup Done or Not

---
+++ Backup_For_App_and_Db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+################ Variables #####################################################
+user="karanravat60@gmail.com"
+DATE="$(date +%d_%m_%Y)"
+DBName="aims_production"
+DBUser="qms"
+Backup_Location="/home/amneal/Backup"
+DocLocation="/home/amneal/Production/jboss-as-7.1.1.Final/docs"
+Message1="Backup Completed for the Amneal  Server"
+Message2="Backup Failed for the Amneal Server-please check"
+
+################# Dump of the database ##################################################
+
+cd "$Backup_Location"
+mkdir "$DATE"
+PGPASSWORD=qms_test pg_dump -U "$DBUser" "$DBName" | gzip  > "$Backup_Location"/"$DATE"/qms-db-"$DATE".sql.gz
+echo "DB backup done"
+################ Create zip of doc folder ##################################################
+cd "$Backup_Location"/"$DATE"
+tar -cvzf "$DATE"_DOCS.tar.gz "$DocLocation" 
+echo "Doc Backup done"
+
+############### To send Mail ##############################
+if [ -e "$Backup_Location"/"$DATE"/"$DATE"_DOCS.zip ] && [ -e "$Backup_Location"/"$DATE"/qms-db-"$DATE".sql.gz ] ; then
+echo "Pass"
+echo "$Message1" | mutt -s "Amneal Backup Status" $user
+else
+echo "fail"
+echo "$Message2" | mutt -s "Amneal Backup Failed" $user
+fi
